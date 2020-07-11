755a449ed992a3cd3d45c9a66c3dbedd8b7125aa
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-07-11 16:33:52 -0400

    Stretch Task completed by implementing Counting Sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,6 +20,7 @@ def selection_sort(arr):
     return arr
 
 
+
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
@@ -37,8 +38,7 @@ print(selection_sort([2,8,5,3,9,4,1]))
 print(bubble_sort([2,8,5,3,9,4,1]))
 
 '''
-STRETCH: implement the Counting 
-Sort function below
+STRETCH: implement the Counting Sort function below
 
 Counting sort is a sorting algorithm that works on a set of data where
 we specifically know the maximum value that can exist in that set of
@@ -47,7 +47,7 @@ from 0 up to the max value. This is most easily done by initializing an
 array of 0s whose length is the max value + 1 (why do we need this "+ 1"?).
 
 Each buckets[i] then is responsible for keeping track of how many times 
-we've seen `i` in the input set of data as we iterate through it.
+we've seen i in the input set of data as we iterate through it.
 Once we know exactly how many times each piece of data in the input set
 showed up, we can construct a sorted set of the input data from the 
 buckets. 
@@ -55,7 +55,40 @@ buckets.
 What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
-    # Your code here
+   
+    if arr:
 
+        # Check negative number
+        if min(arr) < 0:
+            return "Error, negative numbers not allowed in Count Sort"
 
-    return arr
+        # Get maximum number.
+        if maximum is None:
+            maximum = max(arr)
+
+        # Create count array intialize with zero.
+        count_arr = [0] * ( maximum + 1 )
+        
+        # Store the count of each number in count array.
+        for i in range(0, len(arr)):
+            count_arr[arr[i]] += 1   
+
+        # Modify the count array by adding previous counts
+        for i in range(1, len(count_arr)):
+            count_arr[i] += count_arr[i-1] 
+
+        # Place the number on their correct postion and decrease the count by one.
+        sort_arr = [0] * len(arr)
+        
+        for num in arr: 
+            # return num
+            count_arr[num] -= 1
+            sort_arr[count_arr[num]] = num
+            
+        return sort_arr
+    else:
+        return arr    
+
+arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# arr = [3, 2, 1]
+print(counting_sort(arr))
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for index, num in enumerate(arr):
+        if num == target:
+            return index
 
     return -1   # not found
 
@@ -9,6 +11,31 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    arr.sort()
 
+    low, high = 0, len(arr)-1
+    
+    while low <= high:
+        
+        mid = ( low + high ) // 2
+        
+        if target == arr[mid]:
+            return mid
+
+        elif target < arr[mid]:
+            high = mid-1
+
+        elif target > arr[mid]:
+            low = mid +1     
 
     return -1  # not found
+
+# Linear Search
+# arr = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
+# arr = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+# print(linear_search(arr, -6))
+
+# Binary Search
+# arr = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+# arr = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
+# print(binary_search(arr, -6))
